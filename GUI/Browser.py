265af6f15ce4f773848881b5d526334bb42127f5
barna <barnabaskaroly@gmail.com>
2023-03-06 16:58:21 +0100

    aaaaa

--- GUI/Browser.py
+++ GUI/Browser.py
@@ -1,8 +1,23 @@
-import tkinter as tk
+from PyQt5.QtCore import *
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import *
+from PyQt5.QtWebEngineWidgets import *
 
-root = tk.Tk()
+import sys
 
-root.geometry("500x500");
-root.title("my first gui")
+class MainWindow(QMainWindow):
 
-root.mainloop()
+    def __init__(self, *args, **kwargs):
+        super(MainWindow,self).__init__(*args, **kwargs)
+
+        self.browser = QWebEngineView()
+        self.browser.setUrl(QUrl("http://www.google.com"))
+
+        self.setCentralWidget(self.browser)
+
+        self.show()
+
+app = QApplication(sys.argv)
+window = MainWindow()
+
+app.exec_()
